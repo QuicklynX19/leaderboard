00c1be7711591aa3badd7bc8445b8cbfd547d106
Adel Kovačević <kovacevic.adel@gmail.com>
2023-08-15 17:44:58 +0200

    chore: initial project setup (eslint, prettier, typescript)

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import './globals.scss';
+import { Raleway } from 'next/font/google';
+
+const raleway = Raleway({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Leaderboard',
+  description: 'Start your own leaderboard now',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={raleway.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import classes from './page.module.scss';
+
+export default function Home() {
+  return (
+    <main className={classes.container}>
+      <h1>Start your leaderboard</h1>
+    </main>
+  );
+}
